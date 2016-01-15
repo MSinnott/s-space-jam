7913119aab8a698a71549b06d5b93dacf77e2f11
TrostAft <abhijit9331@gmail.com>
2016-01-15 10:14:02 -0500

    Tested byte data. Getting all zeroes. Halp.

--- testing/src/baseRunner.java
+++ testing/src/baseRunner.java
@@ -1,6 +1,11 @@
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Paths;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 
 /**
@@ -8,27 +13,40 @@ import javax.sound.sampled.AudioSystem;
  */
 public class baseRunner {
     
-    public static void main(String[] args){
-        File file = new File("Music/loc.wav"); //grabbing music file
+    public static void main(String[] args) throws IOException {
+        File file = new File("testing/resc/space oddity.au"); //grabbing music file
         byte[] samples;
+        
         //see if this works. I pulled it off of stack overflow
-        AudioInputSteam is = AudioSystem.getAudioInputStream(myFile);
+        AudioInputStream is = null;
+        try {
+            is = AudioSystem.getAudioInputStream(file);
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         DataInputStream dis = new DataInputStream(is);
         try {
             AudioFormat format = is.getFormat();
             samples = new byte[(int)(is.getFrameLength() * format.getFrameSize())];
+//            for (int i = 0; i < samples.length; i++) {
+                    System.out.println(samples.length);
+//            } SO problem. All of the 58286592 bits of samples are zeroes
+            //please note, that with audio, zeroes indicate silence. So maybe something up with file? It plays properly.
             dis.readFully(samples);
+        } catch (IOException e) {
+            e.printStackTrace();
         } finally {
             dis.close();
         }
-        // So this should work by loading the contents of the file into byte using getAudioInputStream
-        //Notice how this wont get header information and metadata, but that probably isn't a problem.
-        //this works for .au audio files
-        try {
-            file.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        //matt's stuff
+//        try {
+//            file.createNewFile();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
     }
         
 }
