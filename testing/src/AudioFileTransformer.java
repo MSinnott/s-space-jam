4b8e62813854694b234f9bba06f1f3cce814fa31
TrostAft <abhijit9331@gmail.com>
2016-01-17 21:32:29 -0500

    I think I made a working DFT.

---
+++ testing/src/AudioFileTransformer.java
@@ -0,0 +1,29 @@
+/**
+ * Created by abhijit on 1/17/16.
+ */
+
+/*Class that handles the transformation of the byte array to s-space from t-space
+*
+* Work in Progress....
+* */
+
+
+public class AudioFileTransformer {
+    /*For Refrence this is how the DFT works*/
+    public static String findDFT(double[] realIn, double[] imaginaryIn, double[] realOut, double[] imaginaryOut) {
+        int n = realIn.length;
+        for (int a = 0; a < n; a++) {
+            double summationReal = 0;
+            double summationImaginary = 0;
+            for(int z = 0; z < n; z++) {
+                double theta = 2 * Math.PI * z * a / n;
+                summationReal += realIn[z] + Math.cos(theta) + imaginaryIn[z] + Math.sin(theta);
+                summationImaginary += -realIn[z] * Math.cos(theta) + imaginaryIn[z] * Math.sin(theta);
+            }
+            realOut[a] = summationReal;
+            imaginaryOut[a] = summationImaginary;
+        }//maybe this works???
+        return realOut + ":" + imaginaryOut;
+    }
+}
+
