0e0d61587870cd9622648c9f34c38cc21e6b53db
MSinnott <msinnott16@gmail.com>
2016-01-27 21:56:07 -0500

    Add basic parsing

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # s-space-jam
 Music generator -- makes new music from previous songs.
+
+Basic multi-document interface allows for editing audio files by hand
+
+Will be adding audio file generation (soon?)
--- testing/src/AdaptiveTextField.java
+++ testing/src/AdaptiveTextField.java
@@ -1,12 +1,18 @@
 import javax.swing.*;
+import java.util.ArrayList;
 
 public class AdaptiveTextField extends JTextField{
+
+    private ArrayList<String> tokens = new ArrayList<String>();
+    private String[] possibleTokens = new String[]{ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "+", "-", "*", "/", "^", "(", ")", "sin(", "cos(", "tan(" };
 
     public AdaptiveTextField(String text){
         super(text);
     }
 
     public float[] generateSamples(int numSamples){
+        tokens = parse(getText());
+
         float[] samples = new float[numSamples];
 
         float scale = (float) (10 * Math.random());
@@ -20,6 +26,24 @@ public class AdaptiveTextField extends JTextField{
 
     public float evalSample(int time){
         return (float) Math.sin(2 * Math.PI * time / 1000);
+    }
+
+    private ArrayList<String> parse(String text){
+        ArrayList<String> parsedTokens = new ArrayList<String>();
+
+        int loc = 0;
+        while (loc < text.length()){
+            for (String possibleToken : possibleTokens) {
+                if (text.indexOf(possibleToken) == loc) {
+                    parsedTokens.add(possibleToken);
+                    loc += possibleToken.length() - 1;
+                    break;
+                }
+            }
+            loc++;
+        }
+
+        return parsedTokens;
     }
 
 }
