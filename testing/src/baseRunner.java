9bf64a7d828fc74f3f3b2ccb23a62cd2d6b7a7ac
MSinnott <msinnott16@gmail.com>
2016-03-23 21:34:40 -0400

    Move audiostream to SoundPlayer class

--- testing/src/baseRunner.java
+++ testing/src/baseRunner.java
@@ -12,47 +12,9 @@ public class baseRunner {
         //making and initializing window --m
         AudioDesktop mainWindow = new AudioDesktop("sSpace -- Music Creator!", 600, 500);
 
-        AudioFileManager aman = generator.genNewComplexSong();
-
         mainWindow.addWindow(new SoundScriptingWindow(mainWindow));
 
-        byte[] soundData = null;
-        ArrayList<byte[]> songs = new ArrayList<byte[]>();
-        final boolean[] addSong = {false};
-        Thread audioThread = new Thread() {
-            @Override
-            public void run() {
-                DataLine.Info info = new DataLine.Info(SourceDataLine.class, aman.getAudioFormat());
-                SourceDataLine sourceLine = null;
-                try {
-                    sourceLine = (SourceDataLine) AudioSystem.getLine(info);
-                    sourceLine.open(aman.getAudioFormat());
-                } catch (LineUnavailableException e){
-                    e.printStackTrace();
-                    System.exit(1);
-                }
-                byte[] toWrite;
-                songs.add(generator.genNewComplexSong().getSoundData());
-                songs.add(generator.genNewComplexSong().getSoundData());
-                sourceLine.start();
-                do {
-                    toWrite = songs.get(0);
-                    sourceLine.write(toWrite, 0, toWrite.length);
-                    Thread.yield();
-                    addSong[0] = true;
-                } while (true);
-            }
-        };
-        audioThread.start();
-        do {
-            while(!addSong[0]){
-                Thread.yield();
-            }
-            soundData = generator.genNewComplexSong().getSoundData();
-            songs.add(soundData);
-            songs.remove(0);
-            addSong[0] = false;
-        } while (true);
+        SoundPlayer soundPlayer = new SoundPlayer();
     }
 
 }
